1914045c9e2bd6230e1e8c812fd3e38778d51167
Bensingh Pancras <bpancras@mathworks.com>
2024-06-19 12:44:43 +0530

    Velocity filter options

--- +pivlab/VelocityFilterOptions.m
+++ +pivlab/VelocityFilterOptions.m
@@ -1,4 +1,4 @@
-classdef VelocityFileringOptions
+classdef VelocityFilterOptions
     %pivlab.VelocityFileringOptions class to create a pivlab options object.
     %   This class contains configuration variables used in velocity
     %   filtering.
@@ -6,9 +6,9 @@ classdef VelocityFileringOptions
     properties (Access=public)
         xCalibrationFactor(1,1) double = 1.0
         yCalibrationFactor(1,1) double = 1.0
-        velocityRange(1,:) double
-        standardDeviation double = nan
-        medianThreshold double = nan
+        VelocityRange(1,:) double
+        StandardDeviation double = nan
+        MedianThreshold double = nan
     end
 end
 
